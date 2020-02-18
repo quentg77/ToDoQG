ca92dae5ac2135cfc3f0a70efa025cf0b0bbe1ec
quentg77 <q.guichaoua@gmail.com>
2020-02-18 14:44:49 +0100

    Changements de configuration OK

--- app/src/main/java/com/example/todoqg/tasklist/Task.kt
+++ app/src/main/java/com/example/todoqg/tasklist/Task.kt
@@ -1,5 +1,10 @@
 package com.example.todoqg.tasklist
 
+import android.os.Parcel
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 import java.io.Serializable
 
-data class Task(var id: String,var title: String, var description: String = " no description"): Serializable
+@Parcelize
+data class Task(var id: String,var title: String, var description: String = " no description") :
+    Parcelable
--- app/src/main/java/com/example/todoqg/tasklist/TaskListFragment.kt
+++ app/src/main/java/com/example/todoqg/tasklist/TaskListFragment.kt
@@ -3,6 +3,7 @@ package com.example.todoqg.tasklist
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,23 +14,40 @@ import com.example.todoqg.R
 import com.example.todoqg.task.TaskActivity
 import kotlinx.android.synthetic.main.fragment_task_list.*
 import java.util.*
+import kotlin.collections.ArrayList
 
-private val taskList = mutableListOf(
-    Task(id = "id_1", title = "Task 1", description = "description 1"),
-    Task(id = "id_2", title = "Task 2"),
-    Task(id = "id_3", title = "Task 3")
-)
 
 const val ADD_TASK_REQUEST_CODE = 123
 const val EDIT_TASK_REQUEST_CODE = 124
 
-class TaskListFragment: Fragment() {
+class TaskListFragment : Fragment() {
+
+    private var taskList = mutableListOf(
+        Task(id = "id_1", title = "Task 1", description = "description 1"),
+        Task(id = "id_2", title = "Task 2"),
+        Task(id = "id_3", title = "Task 3")
+    )
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view: View = inflater.inflate(R.layout.fragment_task_list, container, false)
+
+//        if (savedInstanceState != null) {
+//            with(savedInstanceState) {
+//                var res: ArrayList<Task>? = getParcelableArrayList("list")
+//            }
+//        }
+
+        savedInstanceState?.let {
+            val taskParcelable: ArrayList<Task>? = it.getParcelableArrayList("list")
+
+            if (taskParcelable != null) {
+                taskList = taskParcelable.toMutableList()
+            }
+        }
 
         return view
     }
@@ -40,33 +58,37 @@ class TaskListFragment: Fragment() {
         val taskListAdapter = TaskListAdapter(taskList)
         recyclerView.adapter = taskListAdapter
 
-        taskListAdapter.onDeleteClickListener = {task ->
+        taskListAdapter.onDeleteClickListener = { task ->
             taskList.remove(task)
             recycler.adapter?.notifyDataSetChanged()
         }
 
         FABAdd.setOnClickListener {
-//            taskList.add(Task(id = UUID.randomUUID().toString(), title = "Task ${taskList.size + 1}"))
-//            recycler.adapter?.notifyDataSetChanged()
-
             val intent = Intent(this.context, TaskActivity::class.java)
             startActivityForResult(intent, ADD_TASK_REQUEST_CODE)
         }
 
-        taskListAdapter.onEditClickListener = {task ->
+        taskListAdapter.onEditClickListener = { task ->
             val intent = Intent(this.context, TaskActivity::class.java)
             intent.putExtra("currentTask", task)
             startActivityForResult(intent, EDIT_TASK_REQUEST_CODE)
         }
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putParcelableArrayList("list", ArrayList<Parcelable>(taskList))
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == ADD_TASK_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-            val task = data!!.getSerializableExtra(TaskActivity.TASK_KEY) as Task
+//            val task = data!!.getSerializableExtra(TaskActivity.TASK_KEY) as Task
+            val task = data!!.getParcelableExtra(TaskActivity.TASK_KEY) as Task
             taskList.add(task)
             recycler.adapter?.notifyDataSetChanged()
         } else if (requestCode == EDIT_TASK_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-            val updatedTask = data!!.getSerializableExtra(TaskActivity.TASK_KEY) as Task
+            val updatedTask = data!!.getParcelableExtra(TaskActivity.TASK_KEY) as Task
 //            taskList.replaceAll { t: Task -> if (t.id == updatedTask.id) updatedTask else t}
             val newTask = taskList.find { task -> task.id == updatedTask.id }
             newTask?.title = updatedTask.title
